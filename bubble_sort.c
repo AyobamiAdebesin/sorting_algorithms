3dd5fdf40c3f27a6c920f1b13ddf8c6311bb488a
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-16 19:49:31 +0100

    refactor: another fix

--- bubble_sort.c
+++ bubble_sort.c
@@ -28,21 +28,22 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	bool swapped;
+	/** bool swapped;*/
 
 	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
+		/** swapped = false; */
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				swapped = true;
+				/** swapped = true; */
+				print_array(array, size);
 			}
-			print_array(array, size);
+			
 		}
-		if (swapped == false)
-			break;
+		/** if (swapped == false)
+		break;*/
 	}
 }
