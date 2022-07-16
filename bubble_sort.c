f0e391f66722cdf2be1b09554a31d423a329fabb
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-16 16:19:42 +0100

    finished implementing bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -3,12 +3,21 @@
 /**
  * bubble_sort - Sorts an array of integers in ascending order
  * using the Bubble sort algorithm
- * 
+ *
  *@array: Array of integers to be sorted
  *@size: size of @array
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	return;
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j+1])
+				swap(&array[j], &array[j+1]);
+		}
+	}
 }
