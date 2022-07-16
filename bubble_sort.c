026af1fa891efe4dd3a3763a22ae18215cb1b8b0
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-16 19:52:56 +0100

    refactor: trying to get the required print

--- bubble_sort.c
+++ bubble_sort.c
@@ -28,22 +28,17 @@ void swap(int *xp, int *yp)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
-	/** bool swapped;*/
-
+	
 	for (i = 0; i < size - 1; i++)
 	{
-		/** swapped = false; */
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				swap(&array[j], &array[j + 1]);
-				/** swapped = true; */
 				print_array(array, size);
 			}
 			
 		}
-		/** if (swapped == false)
-		break;*/
 	}
 }
