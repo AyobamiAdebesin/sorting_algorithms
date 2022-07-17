92e9093595a77be08db0c11ed052b7562037c059
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-17 08:53:01 +0100

    Added new file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - Sorts a diuble linked list of integers in
+ * ascending order using the insertion sort algorithm
+ *
+ *
+ * @list: double pointer to the head node of a doubly linked list
+ * Return: 0
+ */
+
+void insertion_sort_list(listint_t, **list):
+{
+	return;
+}
