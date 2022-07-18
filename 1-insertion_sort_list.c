eb9fa7f5984b4c35fedb7432beaf717b34793065
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-18 02:26:36 +0100

    refactor: Implemented inswertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -10,5 +10,35 @@
 
 void insertion_sort_list(listint_t, **list):
 {
-	return;
+	size_t size, i, j;
+	int key;
+
+	listint_t *tmp, *h;
+
+	tmp = *list;
+	h = *list;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+		{
+			h = h->prev;
+		}
+	}
+
+	
+	/** Get the size of the list*/
+	tmp = h;
+	size = 0;
+	while (tmp != NULL)
+	{
+		size++;
+		tmp = tmp->next;
+	}
+
+	for (i = 1; i < size; i++)
+	{
+
+
+
 }
