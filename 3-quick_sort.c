c5c3c00a8c5608675adb51b95f05fc2670a95d5a
Dolzy <dolzy@LAPTOP-SOLC3NM9.localdomain>
2022-07-20 00:24:06 +0100

    code quick-sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,3 +15,21 @@ void swap(int *xp, int *yp)
 	*yp = temp;
 }
 
+void quick_sort(int *array, size_t size)
+{
+	int i; 
+	int j = 0;
+	size_t pivot = size - 1;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			swap(&array[i], &array[j]);
+			/**print_array(array, size);**/
+			j++;
+		}
+	}
+	swap(&array[j], &array[pivot]);
+	/**print_array(array, size);**/
+}
