ab015a882e9de12ad0b82164cf8a26707b8e3129
Dolzy <dolzy@LAPTOP-SOLC3NM9.localdomain>
2022-07-20 01:16:40 +0100

    remodified quick_sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -15,21 +15,73 @@ void swap(int *xp, int *yp)
 	*yp = temp;
 }
 
-void quick_sort(int *array, size_t size)
-{
-	int i; 
-	int j = 0;
-	size_t pivot = size - 1;
+/**
+ * partition - function that sorts an array of integers
+ * and get the index of the partition.
+ * @A: array of integers
+ * @size: lenght of array
+ * @low_index: low index
+ * @high_index: high index
+ * Return: none
+ */
 
-	for (i = 0; i < size - 1; i++)
+ssize_t partition(int *Arr, ssize_t low, ssize_t high, size_t size)
+{
+
+	/**The pivot of the array**/
+	int pivot = Arr[high];
+	int i;  /**elements of the array**/
+	int j = 0; /**Counter variable for partition**/
+
+	for (i = 0; i < high; i++)
 	{
-		if (array[i] <= pivot)
+		if (Arr[i] <= pivot);
 		{
-			swap(&array[i], &array[j]);
-			/**print_array(array, size);**/
+			swap(&Arr[i], &Arr[j]);
+			print_array(Arr, size);
 			j++;
 		}
 	}
-	swap(&array[j], &array[pivot]);
-	/**print_array(array, size);**/
+	if (Arr[j] != Arr[high])
+	{
+    swap(&Arr[j], &Arr[pivot]);
+	print_array(Arr, size);
+	}
+	return (j);
+}
+
+/**
+ * quicksort2 - function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm
+ * @A: array of integers
+ * @size: lenght of array
+ * @low_index: low index
+ * @high_index: high index
+ * Return: none
+ */
+
+void quicksort2(int *Arr, ssize_t low, ssize_t high, size_t size)
+{
+	ssize_t par = 0;
+
+	if (low  < high)
+	{
+		/**Getting the index of the partition**/
+		par = partition(Arr, low, high, size);
+		/**Quicksorting the leftside of the partition**/
+		quicksort2(Arr, low, par - 1, size);
+		/**Quicksorting the rightside of the partition**/
+		quicksort2(Arr, par + 1, high, size);
+	}
+}
+
+
+
+void quick_sort(int *array, size_t size)
+{
+	if (!array || size < 2)
+	{
+		return;
+	}
+	quicksort2(array, 0, size - 1, size);
 }
