2079ff70a3b2e645e56d56aa55daf35818f499b2
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-20 05:00:39 +0100

    refactor: fixed selection sort program

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,3 +1,28 @@
+#include "sort.h"  
+
+/**
+ * swap - Swaps two integer pointers
+ *
+ * @xp: first integer pointer
+ * @yp: second integer pointer
+ *
+ * Return: None
+ */
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
+ * selection_sort - sorts an array by holding the righmost value
+ * and swaping with the least value in the rest of the array.
+ * 
+ * @array: Array of integer to be sorted
+ * @size: size of @array
+ */
+
 void selection_sort(int *array, size_t size)
 {
 	size_t i, index;
