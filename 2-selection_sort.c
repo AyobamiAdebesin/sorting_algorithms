05267f191868e44e4890714555e5f3a4f371f850
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-20 04:35:17 +0100

    refactor: implemented selection sort all from scratch

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,47 +1,34 @@
-#include "sort.h"  
+#include "sort.h"
 
 /**
- * swap - Swaps two integer pointers
+ * selection_sort - sorts an array of integers in
+ * ascending order using the Selection sort
+ * algorithm
  *
- * @xp: first integer pointer
- * @yp: second integer pointer
- *
- * Return: None
+ * @array: input array of integers
+ * @size: size of the array
+ * Return: no return
  */
-void swap(int *xp, int *yp)
-{
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
-}
-
-/**
- * selection_sort - sorts an array by holding the righmost value
- * and swaping with the least value in the rest of the array.
- * 
- * @array: Array of integer to be sorted
- * @size: size of @array
- */
-
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_index;
+	int tmp;
+	size_t i, j, m;
 
-    for(i = 0; i < (size - 1); i++)
-    {
-        min_index = i;
+	for (i = 0; i < size; i++)
+	{
+		m = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[m] > array[j])
+				m = j;
+		}
 
-        for(j = i + 1; j < size; j++)
-        {
-            if (array[j] < array[min_index])
-            {
-                min_index = j;
-            }
-        }
-        if (min_index != i)
-        {
-            swap(&array[i], &array[min_index]);
-            print_array(array, size);
-        }
-    }
+		if (i != m)
+		{
+			tmp = array[i];
+			array[i] = array[m];
+			array[m] = tmp;
+			print_array(array, size);
+		}
+	}
 }
