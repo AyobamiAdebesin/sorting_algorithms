5dacf9365aad31f6e202cc7b242b95c06e404adb
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-20 04:46:55 +0100

    refactor: fixed function naming error

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,10 +10,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	_qsort(array, 0, size - 1, size);
+	qsort1(array, 0, size - 1, size);
 }
 /**
- * _qsort - auxiliar function for the
+ * qsort1 - auxiliar function for the
  * quick_sort function
  * @a: input arrray
  * @low: index for the first element
@@ -21,7 +21,7 @@ void quick_sort(int *array, size_t size)
  * @size: size of the array
  * Return: no return
  */
-void _qsort(int *a, int low, int high, int size)
+void qsort1(int *a, int low, int high, int size)
 {
 	int p, w, i;
 	int tmp;
@@ -51,7 +51,7 @@ void _qsort(int *a, int low, int high, int size)
 			a[p] = tmp;
 			print_array(a, size);
 		}
-		_qsort(a, low, w - 1, size);
-		_qsort(a, w + 1, high, size);
+		qsort1(a, low, w - 1, size);
+		qsort1(a, w + 1, high, size);
 	}
 }
