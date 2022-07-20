29182b5f018f6adfbec7dc84bac2f8e169f9abcc
AyobamiAdebesin <froshofficial@gmail.com>
2022-07-20 04:47:34 +0100

    refactor: added prototype for quick sort

--- sort.h
+++ sort.h
@@ -29,5 +29,9 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void qsort1(int *a, int low, int high, int size);
 void swap(int *xp, int *yp);
+
+
+
 #endif
